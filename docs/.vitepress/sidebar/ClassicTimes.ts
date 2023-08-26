38270540498f716055bb32886126b3ee10ca5087
yeshan-taoist <mypotato90@outlook.com>
2023-08-26 12:28:03 +0800

    Create 文学之路

---
+++ docs/.vitepress/sidebar/ClassicTimes.ts
@@ -0,0 +1,38 @@
+// 古典文学目录
+export const ClassicTimes = [
+  {
+    text: "唐诗",
+    collapsed: true,
+    items: [],
+  },
+  {
+    text: "宋词",
+    collapsed: true,
+    items: [],
+  },
+  {
+    text: "元曲",
+    collapsed: true,
+    items: [],
+  },
+  {
+    text: "散文",
+    collapsed: true,
+    items: [],
+  },
+  {
+    text: "小说",
+    collapsed: true,
+    items: [],
+  },
+  {
+    text: "史书",
+    collapsed: true,
+    items: [],
+  },
+  {
+    text: "杂谈",
+    collapsed: true,
+    items: [],
+  },
+];
---
+++ docs/.vitepress/sidebar/RecentTimes.ts
@@ -0,0 +1,9 @@
+// 现代文学目录
+
+export const RecentTimes = [
+  {
+    text: "小说集",
+    collapsed: true,
+    items: [],
+  },
+];
